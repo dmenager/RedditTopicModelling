6472122a123cc826c459a1479953ecd2ed279305
Chris Seasholtz <cdseasholtz@ku.edu>
2016-11-29 12:29:42 -0600

    Increased max_df and created a hash table.
    Using a max_df of .9 would not work with larger datasets, so I increased
    it to 5 and it worked. I also created a hash table where the key is the
    subreddit name and the entry are the words from the LDA function.
    2016-08-15000 is 15000 entries from the 2016-08 dataset, likewise with
    2019-09-15000.

--- topicModel.py
+++ topicModel.py
@@ -4,11 +4,13 @@ from time import time
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
-def print_top_words(model, feature_names, n_top_words):
+
+def print_top_words(sub_name, model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
         print("Topic #%d:" % topic_idx)
-        print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
+        results[sub_name] = " ".join([feature_names[i]
+                        for i in topic.argsort()[:-n_top_words - 1:-1]])
+        print(results[sub_name])
     print()
 
 
@@ -17,13 +19,13 @@ def fit_LDA(data_samples, n_samples, topics, name):
     n_topics = topics
     n_top_words = 10
     # Use tf (raw term count) features for LDA.
-    print("n_samples=%d" %(n_samples))
+    print("n_samples=%d" % (n_samples))
     print("Extracting tf features for LDA...")
-    tf_vectorizer = CountVectorizer(max_df=.9, min_df=1,
+    tf_vectorizer = CountVectorizer(max_df=5, min_df=1,
                                     max_features=None,
                                     stop_words='english')
 
-    #tf_vectorizer = CountVectorizer(max_df=.3, min_df=1,
+    # tf_vectorizer = CountVectorizer(max_df=.3, min_df=1,
     #                                max_features=None,
     #                                stop_words='english')
     # Pass in a list of strings here
@@ -39,7 +41,8 @@ def fit_LDA(data_samples, n_samples, topics, name):
 
     print("\nTopics in LDA model:")
     tf_feature_names = tf_vectorizer.get_feature_names()
-    print_top_words(lda, tf_feature_names, n_top_words)
+    print_top_words(name, lda, tf_feature_names, n_top_words)
+
 
 def explore_data(file):
     data = None
@@ -50,29 +53,29 @@ def explore_data(file):
         data = f.readlines()
 
     # Remove excess
-    data = map(lambda x : x.rstrip(), data)
+    data = map(lambda x: x.rstrip(), data)
     data_json_str = "[" + ','.join(data) + "]"
 
     # read in pandas
     data = pd.read_json(data_json_str)
-    
-    # set the index to be this and don't drop
-    data.set_index(keys=['subreddit'], drop=False,inplace=True)
-    # get a list of names
-    subreddits=data['subreddit'].unique().tolist()
 
-    #samples = [x for x in data['body']]
-    #fit_LDA(samples, len(samples), len(subreddits))
-    
+    # set the index to be this and don't drop
+    data.set_index(keys=['subreddit'], drop=False, inplace=True)
+    # get a list of names
+    subreddits = data['subreddit'].unique().tolist()
+
+    # samples = [x for x in data['body']]
+    # fit_LDA(samples, len(samples), len(subreddits))
+    print(subreddits)
     for idx, subredditName in enumerate(subreddits):
-        subDF = data.loc[data.subreddit==subreddits[idx]]
-        #print subDF
+        subDF = data.loc[data.subreddit == subreddits[idx]]
+        # print subDF
         # shuffle data
-        subDF = subDF.sample(frac = 1).reset_index(drop=True)
+        subDF = subDF.sample(frac=1).reset_index(drop=True)
         samples = [x for x in subDF['body']]
         if len(samples) > 1:
             fit_LDA(samples, len(samples), 1, subredditName)
- 
-    
 
-explore_data('sample.json')
+results = {}
+explore_data('2016-08-15000.json')
+print(results)
